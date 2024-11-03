dac7a77223bb106d1543370a4afdf1733287583b
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2024-11-03 18:34:51 +0530

    Time: 0 ms (100%), Space: 20.8 MB (38.3%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        
+        // Calculate the sum of the first n natural numbers
+        int sum_n = n * (n + 1) / 2;
+        
+        // Calculate the sum of the numbers in nums
+        int sum_nums = 0;
+        for (int num : nums) {
+            sum_nums += num;
+        }
+        
+        // The missing number is the difference between sum_n and sum_nums
+        return sum_n - sum_nums;
+    }
+};
