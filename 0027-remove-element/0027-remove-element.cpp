9cd56203a51ca589d6a00ed94a5841c697758f03
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2024-11-08 21:55:11 +0530

    Time: 0 ms (100%), Space: 10.8 MB (36.08%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0; // `k` will count elements not equal to `val`
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i]; // Move the element to the position `k`
+                k++; // Increment the count of elements not equal to `val`
+            }
+        }
+        
+        return k;
+    }
+};
