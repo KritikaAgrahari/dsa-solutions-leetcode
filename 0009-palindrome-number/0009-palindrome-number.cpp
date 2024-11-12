d560f8638c0e829a352d27f45f6135244a22ef11
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2024-11-12 17:09:09 +0530

    Time: 0 ms (100%), Space: 8.6 MB (38.82%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,20 @@
+class Solution { //The O(log₁₀(x)) time complexity is already the most optimal for this problem because you have to process a certain number of digits to determine whether x is a palindrome.The space complexity is O(1) because we only use a few integer variables
+public:
+    bool isPalindrome(int x) {
+        // If x is negative or ends in 0 (but isn't 0), it's not a palindrome
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedHalf = 0;
+        // Reverse the last half of the number
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        // Check if the first half is equal to the reversed second half
+        // Also, consider the case when the number has an odd number of digits
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+};
