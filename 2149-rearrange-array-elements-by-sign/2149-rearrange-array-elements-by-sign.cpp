0e44026477ada19983259f20cb122afbe7e1a3cf
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2024-11-20 17:37:28 +0530

    Time: 3 ms (87.06%), Space: 126.6 MB (54.85%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int posIndex = 0;          // Index for placing positive numbers
+        int negIndex = 1;          // Index for placing negative numbers
+        int n = nums.size();       // Get the size of the array
+        vector<int> result(n);     // Create a result array of the same size
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < 0) {     // If the current number is negative
+                result[negIndex] = nums[i];  // Place the negative number
+                negIndex += 2;      // Move to the next position for negatives
+            } else {                // If the current number is positive
+                result[posIndex] = nums[i];  // Place the positive number
+                posIndex += 2;      // Move to the next position for positives
+            }
+        }
+
+        return result;              // Return the rearranged array
+    }
+};
