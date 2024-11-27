9ef272570b89b7cdc139365a36d927dece2c2073
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2024-11-27 22:12:05 +0530

    Time: 0 ms (100%), Space: 9 MB (8.69%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> row(rowIndex + 1, 1); // Initialize a row with all 1s
+        
+        // Compute values based on previous row's values
+        for (int i = 1; i <= rowIndex; ++i) {
+            // Start from the end to avoid overwriting values we still need
+            for (int j = i - 1; j > 0; --j) {
+                row[j] = row[j] + row[j - 1];
+            }
+        }
+        
+        return row;
+    
+        
+    }
+};
