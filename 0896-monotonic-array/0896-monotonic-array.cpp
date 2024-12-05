5ef363b1bef1206c7636260591c293a8dd6c667d
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2024-12-05 22:59:39 +0530

    Time: 4 ms (21.29%), Space: 100.6 MB (6.71%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+         bool increasing = true, decreasing = true;  // Flags to check if the array is increasing or decreasing
+        int n = nums.size();  // Get the size of the array
+
+        // Traverse the array from index 1 (the second element) to the end
+        for (int i = 1; i < n; ++i) {
+            
+            if (nums[i] > nums[i - 1]) {
+                decreasing = false; // If we find an increasing pair, it can't be decreasing
+            }
+            if (nums[i] < nums[i - 1]) {
+                increasing = false; // If we find a decreasing pair, it can't be increasing
+            }
+
+            // If both flags are false, it means the array is neither increasing nor decreasing
+            if (!increasing && !decreasing) {
+                return false; // No point in checking further, the array is not monotonic
+            }
+        }
+
+        // If the loop finishes, that means the array is either increasing or decreasing (monotonic)
+        return true;
+    
+    }
+};
