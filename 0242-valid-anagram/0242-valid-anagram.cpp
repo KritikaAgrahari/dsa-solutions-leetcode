39389112e94f865a453d43cdf4dbe5de63926b56
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2024-12-16 18:02:07 +0530

    Time: 8 ms (14.59%), Space: 9.6 MB (50.16%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,45 +1,44 @@
-// class Solution { //tc is 0(nlog n)
-// public:
-//     bool isAnagram(string s, string t) {
-        
-//         if (s.length()!=t.length()){
-//             return false;
-//         }
-//           sort(s.begin(),s.end());
-//         sort(t.begin(),t.end());
-//         for(int i=0; i<s.length(); i++){
-//             if(s[i]!=t[i]){
-//                 return false;
-//             }
-//         }
-//         return true;
-//             }
-// };
-
-
-class Solution { //tc is 0(n)
+class Solution { //tc is 0(nlog n)
 public:
     bool isAnagram(string s, string t) {
-         if (s.length()!=t.length()){
-             return false;
+        
+        if (s.length()!=t.length()){
+            return false;
         }
-
-        int freq[26]={0};
-        for (int i=0; i<s.length();i++){
-            freq[s[i]-'a']++;
-
-        }
-         for (int i=0; i<t.length();i++){
-            freq[t[i]-'a']--;
-
-        }
-        for (int i=0; i<26; i++){
-            if (freq[i]!=0){
+          sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        for(int i=0; i<s.length(); i++){
+            if(s[i]!=t[i]){
                 return false;
             }
-           
         }
-         return true;
-
-    }
+        return true;
+            }
 };
+
+// class Solution { //tc is 0(n)
+// public:
+//     bool isAnagram(string s, string t) {
+//          if (s.length()!=t.length()){
+//              return false;
+//         }
+
+//         int freq[26]={0};
+//         for (int i=0; i<s.length();i++){
+//             freq[s[i]-'a']++;
+
+//         }
+//          for (int i=0; i<t.length();i++){
+//             freq[t[i]-'a']--;
+
+//         }
+//         for (int i=0; i<26; i++){
+//             if (freq[i]!=0){
+//                 return false;
+//             }
+           
+//         }
+//          return true;
+
+//     }
+// };
