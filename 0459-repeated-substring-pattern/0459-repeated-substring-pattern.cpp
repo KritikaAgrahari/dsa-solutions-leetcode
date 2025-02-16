f8e71e2adfa303ae5be4ba4230cbda6299bc7e81
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2025-02-16 21:41:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,36 @@
+// class Solution {
+// public:
+//     bool repeatedSubstringPattern(string s) {
+//         int n= s.length();
+//         for (int i=n/2; i>=1;i--){
+//             if (n%i==0){
+//                 int times= n/i;
+//                 string pattern = s.substr(0,i);
+//                 string newptn = "";
+                
+            
+//             while (times--){
+//                 newptn += pattern;
+//             }
+            
+//             if (newptn == s){
+//                 return true;
+//              } }
+//         }
+//         return false;
+//     }
+// };
+
+
+
+//EASIEST WAY TO APPROACH 
+// class Solution {
+// public:
+//     bool repeatedSubstringPattern(string s) {
+//         string doubled = s + s;  // Double the string
+//         return doubled.substr(1, doubled.size() - 2).find(s) != string::npos;
+//     }
+// };
+
+
+
