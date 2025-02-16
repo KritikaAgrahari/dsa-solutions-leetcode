787e9c8be8ee7b4862fcb08795f9d5c31c1fd53f
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2025-02-16 21:41:10 +0530

    Time: 19 ms (50.51%), Space: 47.6 MB (43.05%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,25 +1,25 @@
-// class Solution {
-// public:
-//     bool repeatedSubstringPattern(string s) {
-//         int n= s.length();
-//         for (int i=n/2; i>=1;i--){
-//             if (n%i==0){
-//                 int times= n/i;
-//                 string pattern = s.substr(0,i);
-//                 string newptn = "";
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n= s.length();
+        for (int i=n/2; i>=1;i--){
+            if (n%i==0){
+                int times= n/i;
+                string pattern = s.substr(0,i);
+                string newptn = "";
                 
             
-//             while (times--){
-//                 newptn += pattern;
-//             }
+            while (times--){
+                newptn += pattern;
+            }
             
-//             if (newptn == s){
-//                 return true;
-//              } }
-//         }
-//         return false;
-//     }
-// };
+            if (newptn == s){
+                return true;
+             } }
+        }
+        return false;
+    }
+};
 
 
 
