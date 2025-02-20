acb7fa0d7a486fc21294dd3217adaeee956d1112
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2025-02-20 23:03:17 +0530

    Time: 11 ms (20.17%), Space: 17.3 MB (30.91%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -5,7 +5,7 @@ public:
         int black=0;
         
         long long swap=0;
-        for (int i=0; i<=n; i++){
+        for (int i=0; i<n; i++){
             if (s[i]=='0'){
                 swap += black;
 
