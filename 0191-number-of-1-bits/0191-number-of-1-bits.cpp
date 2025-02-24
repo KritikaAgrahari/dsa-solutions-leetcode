02bf23bdde5acb7f8f544c0eb27143bd85637871
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2025-02-24 16:50:09 +0530

    Time: 0 ms (100%), Space: 8.1 MB (80.58%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,12 +1,22 @@
+// class Solution {
+// public:
+//     int hammingWeight(int n) {
+//         int count = 0;
+//         while(n){
+//             count += n%2;
+//             n=n/2;
+//         }
+
+//         return count;
+//     }
+// };
+
+
 class Solution {
 public:
     int hammingWeight(int n) {
-        int count = 0;
-        while(n){
-            count += n%2;
-            n=n/2;
-        }
+       
 
-        return count;
+        return __builtin_popcount(n);
     }
 };
