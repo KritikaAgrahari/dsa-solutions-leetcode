cdba611b3eaadd33e6cc2e1ed717bdf5231abba9
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2025-02-24 17:03:54 +0530

    Time: 0 ms (100%), Space: 8 MB (44.41%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,16 +1,25 @@
+// class Solution {
+// public:
+//     int numberOfSteps(int num) {
+//         int count =0;
+//         while (num>0){
+//             if (num % 2 == 0){
+//                 num /=2;
+//             }
+//             else{
+//                 num -=1;
+//             }
+//             count++;
+//         }
+//         return count;
+//     }
+// };
+
+
 class Solution {
 public:
     int numberOfSteps(int num) {
-        int count =0;
-        while (num>0){
-            if (num % 2 == 0){
-                num /=2;
-            }
-            else{
-                num -=1;
-            }
-            count++;
-        }
-        return count;
+        if (num == 0) return 0;
+        return __builtin_popcount(num) + 31 - __builtin_clz(num);
     }
 };
