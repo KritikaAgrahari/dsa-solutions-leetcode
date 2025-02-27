577086d4ff54fe6df917f68d071c815e36f3825b
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2025-02-27 20:56:06 +0530

    Time: 0 ms (100%), Space: 7.7 MB (64.63%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+        for(int i=0; i<n; i++){
+            if(n%2==0){
+                return true;
+            }
+        }
+            return false;
+
+    }
+
+};
