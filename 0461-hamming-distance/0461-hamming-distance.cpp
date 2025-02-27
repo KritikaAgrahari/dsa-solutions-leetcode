04df28850030624794cc456290f342f755856f81
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2025-02-27 21:53:56 +0530

    Time: 0 ms (100%), Space: 7.9 MB (67.77%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.cpp
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -1,14 +1,21 @@
+// class Solution {
+// public:
+//     int hammingDistance(int x, int y) {
+//         int result = (x ^ y);
+//         int count = 0;  // Initialize count
+
+//         while (result != 0) {
+//             count += result & 1;  // Check last bit
+//             result >>= 1;   // Right shift result
+//         }
+
+//         return count;  // Return count instead of result
+//     }
+// };
+
 class Solution {
 public:
     int hammingDistance(int x, int y) {
-        int result = (x ^ y);
-        int count = 0;  // Initialize count
-
-        while (result != 0) {
-            count += result & 1;  // Check last bit
-            result >>= 1;   // Right shift result
-        }
-
-        return count;  // Return count instead of result
+        return __builtin_popcount(x ^ y);  // Counts the number of 1s in (x XOR y)
     }
 };
