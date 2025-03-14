a8e1800b85be5ca10af81f46b24ce2662b4928e3
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2025-03-14 17:19:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maximumCandies(vector<int>& candies, long long k) {
+        int ans;
+        sort(candies.begin(),candies.end());
+        for (int i=0; i<candies.size(); i++){
+            ans=candies[0];
+
+        }
+    return ans;
+        
+    }
+};
