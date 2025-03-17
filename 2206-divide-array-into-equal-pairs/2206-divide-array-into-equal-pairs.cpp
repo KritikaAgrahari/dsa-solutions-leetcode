cddb69dc06dfba103217fd5228585beccb2f1cb6
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2025-03-17 08:48:43 +0530

    Time: 0 ms (100%), Space: 16.5 MB (92.45%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
@@ -1,13 +1,25 @@
+// class Solution {
+// public:
+//     bool divideArray(vector<int>& nums) {
+//         unordered_map<int,int>frequency;
+//         for (int i=0; i<nums.size(); i++){
+//             frequency[nums[i]]++;
+//         }
+
+//         for (auto it = frequency.begin(); it != frequency.end(); ++it) {
+//             if(it->second % 2!=0) return false;
+//         }
+//         return true;
+//     }
+// };
+
+
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
-        unordered_map<int,int>frequency;
-        for (int i=0; i<nums.size(); i++){
-            frequency[nums[i]]++;
-        }
-
-        for (auto it = frequency.begin(); it != frequency.end(); ++it) {
-            if(it->second % 2!=0) return false;
+        sort(nums.begin(), nums.end()); // O(N log N)
+        for (int i = 0; i < nums.size(); i += 2) { // O(N)
+            if (nums[i] != nums[i + 1]) return false;
         }
         return true;
     }
