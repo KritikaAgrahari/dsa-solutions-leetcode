2912f6b566bfcd1ca2789d09db4ccc15f217f031
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2025-03-20 22:23:37 +0530

    Time: 17 ms (75.65%), Space: 207.9 MB (66.23%) - LeetHub

--- 3108-minimum-cost-walk-in-weighted-graph/3108-minimum-cost-walk-in-weighted-graph.cpp
+++ 3108-minimum-cost-walk-in-weighted-graph/3108-minimum-cost-walk-in-weighted-graph.cpp
@@ -1,60 +1,61 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-class DSU {
-public:
-    vector<int> parent, rank;
-    
-    DSU(int n) {
-        parent.resize(n);
-        rank.resize(n, 1);
-        for (int i = 0; i < n; i++) parent[i] = i;
-    }
-    
-    int find(int x) {
-        if (parent[x] != x) parent[x] = find(parent[x]); // Path compression
-        return parent[x];
-    }
-    
-    void unite(int x, int y) {
-        int px = find(x), py = find(y);
-        if (px != py) {
-            if (rank[px] > rank[py]) swap(px, py);
-            parent[px] = py;
-            if (rank[px] == rank[py]) rank[py]++;
-        }
-    }
-};
-
 class Solution {
 public:
-    vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& queries) {
-        sort(edges.begin(), edges.end(), [](auto &a, auto &b) { return a[2] < b[2]; });
-        
-        unordered_map<int, DSU> componentMap;
-        for (auto &e : edges) {
-            int u = e[0], v = e[1], w = e[2];
-            if (componentMap.find(w) == componentMap.end()) 
-                componentMap[w] = DSU(n);
-            componentMap[w].unite(u, v);
-        }
 
-        vector<int> res;
-        for (auto &q : queries) {
-            int s = q[0], t = q[1];
-            if (s == t) {
-                res.push_back(0);
-                continue;
-            }
+    vector<int> parent;
+    int find(int x) {
+        if(parent[x] == x)
+            return x;
+        
+        return parent[x] = find(parent[x]);
+    }
+    void Union(int x, int y) {
+        parent[y] = x;
+    }
+
+    vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
+        
+        parent.resize(n);
+        vector<int> cost(n);  //Store "And operation" cost of each component
+	    
+	    for(int i = 0; i<n; i++) {
+	        parent[i] = i;
+            cost[i]   = -1;
+        }
+        
+        for (auto &edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
             
-            int minAnd = -1;
-            for (auto &[w, dsu] : componentMap) {
-                if (dsu.find(s) == dsu.find(t)) {
-                    minAnd = (minAnd == -1) ? w : min(minAnd, w);
-                }
+            int parent_u = find(u);
+            int parent_v = find(v);
+            
+
+            if (parent_u != parent_v) {
+                cost[parent_u] &= cost[parent_v];
+                Union(parent_u, parent_v);
             }
-            res.push_back(minAnd);
+
+            cost[parent_u] &= w;
+        }
+        
+        vector<int> res;
+        for (auto &q : query) {
+            int s = q[0];
+            int t = q[1];
+            
+            int p1 = find(s);
+            int p2 = find(t);
+            
+            if (s == t)
+                res.push_back(0);
+            else if (p1 != p2)
+                res.push_back(-1);
+            else
+                res.push_back(cost[p1]);
         }
         return res;
     }
 };
+
+
