67553f4193979d40093f85d8807762f4b358e3c8
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2025-06-22 18:14:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        vector<int> mp(k, 0);
+
+        // Count frequency of each remainder
+        for (int i = 0; i < arr.size(); i++) {
+            int rem = (arr[i] % k + k) % k;
+            mp[rem]++;
+        }
+
+        // Remainder 0 must be even
+        if (mp[0] % 2 != 0) return false;
+
+        // Check pairs of remainders
+        for (int rem = 1; rem < k/2; rem++) {
+            if (mp[rem] != mp[k - rem]) return false;
+        }
+
+        return true;
+    }
+};
