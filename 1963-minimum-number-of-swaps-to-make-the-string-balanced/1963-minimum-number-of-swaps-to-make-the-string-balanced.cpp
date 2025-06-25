80f09b4130132ce2752c2ea609e728c54535390d
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2025-06-25 21:20:19 +0530

    Time: 27 ms (18.97%), Space: 36.8 MB (28.03%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,18 +1,38 @@
+// class Solution {
+// public:
+//     int minSwaps(string s) {
+
+//         int count=0;
+//         for(int i=0; i<s.length(); i++){
+           
+//             if(s[i]=='['){
+//                 count++;
+//             }
+//             else if(count>0){
+//                 count--;
+//             }
+//         }
+//         return (count+1)/2;
+        
+//     }
+// };
+
+
 class Solution {
 public:
     int minSwaps(string s) {
 
-        int count=0;
-        for(int i=0; i<s.length(); i++){
-           
-            if(s[i]=='['){
-                count++;
-            }
-            else if(count>0){
-                count--;
-            }
+        stack<char>st;
+        int n=s.length();
+        for(int i=0; i<n ; i++){
+if(s[i]=='['){
+    st.push(s[i]);
+}
+
+else if(!st.empty()){
+    st.pop();
+}
         }
-        return (count+1)/2;
-        
+        return (st.size()+1)/2;
     }
-};
+    };
