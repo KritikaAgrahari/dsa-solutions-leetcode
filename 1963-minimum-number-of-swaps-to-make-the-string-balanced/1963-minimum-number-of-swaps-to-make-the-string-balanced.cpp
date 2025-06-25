236000a44b2d1e7965599169812b4b2edd14cfd6
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2025-06-25 21:18:45 +0530

    Time: 6 ms (92.24%), Space: 34.2 MB (60.6%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -4,11 +4,12 @@ public:
 
         int count=0;
         for(int i=0; i<s.length(); i++){
-            if(s[i]=='['  && s[i+1]==']'){
-                continue;
-            }
-            else if(s[i]=='['){
+           
+            if(s[i]=='['){
                 count++;
+            }
+            else if(count>0){
+                count--;
             }
         }
         return (count+1)/2;
