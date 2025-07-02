e43c37649f87d27b2899857b68d1b4a2da5ef196
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2025-07-02 22:59:25 +0530

    Time: 17 ms (97.28%), Space: 57.3 MB (23.32%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
@@ -1,16 +1,33 @@
+// class Solution {
+// public:
+//     int maxCoins(vector<int>& piles) {
+//         int n = piles.size();
+//         int result = 0;
+//         sort(piles.begin(), piles.end());
+//         int b=0;
+//         int m=n-2;
+//         while(m>b){
+//           result += piles[m];
+//             m -=2;
+//             b++;
+//         }
+//         return result;
+        
+//     }
+// };
+
+
+
 class Solution {
 public:
     int maxCoins(vector<int>& piles) {
         int n = piles.size();
         int result = 0;
         sort(piles.begin(), piles.end());
-        int b=0;
-        int m=n-2;
-        while(m>b){
-          result += piles[m];
-            m -=2;
-            b++;
-        }
+       for(int m=n/3; m<n ; m+=2){
+        result += piles[m];
+       }
+        
         return result;
         
     }
