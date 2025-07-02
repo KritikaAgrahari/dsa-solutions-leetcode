b6703c5a1897f966cd24b6f8c79dcc0c2d3a119d
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2025-07-02 22:33:41 +0530

    Time: 177 ms (34.37%), Space: 117.4 MB (5.1%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -1,20 +1,47 @@
+// class Solution {
+// public:
+//     long long maximumHappinessSum(vector<int>& happiness, int k) {
+        
+//         sort(begin(happiness), end(happiness), greater<int>());
+
+//         long long result = 0;  
+
+//         int count = 0;  
+
+        
+//         for (int i = 0; i < k; i++) {
+//             result += max(happiness[i] - count, 0);
+
+//             count++;
+//         }
+
+//         return result; 
+//     }
+// };
+
+
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
-        
-        sort(begin(happiness), end(happiness), greater<int>());
+        long long result = 0;   
+        int count = 0;          
 
-        long long result = 0;  
+        priority_queue<int> pq; 
 
-        int count = 0;  
-
-        
-        for (int i = 0; i < k; i++) {
-            result += max(happiness[i] - count, 0);
-
-            count++;
+        // Traditional for loop to insert all elements into the max-heap
+        for (int i = 0; i < happiness.size(); i++) {
+            pq.push(happiness[i]);  
         }
 
-        return result; 
+        for (int i = 0; i < k; i++) {
+            int hap = pq.top();    
+            pq.pop();               
+
+            result += max(hap - count, 0);
+
+            count++;               
+        }
+
+        return result;  
     }
 };
