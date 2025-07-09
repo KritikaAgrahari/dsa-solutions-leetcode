e6b16a3a1b6bc31c04d7e81fd7a2df782750bf74
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2025-07-09 17:06:29 +0530

    Time: 0 ms (100%), Space: 9.3 MB (79.64%) - LeetHub

--- 3442-maximum-difference-between-even-and-odd-frequency-i/3442-maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3442-maximum-difference-between-even-and-odd-frequency-i/3442-maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     int maxDifference(string s) {
-        int n=s.length();
+        int n = s.length();
 
-        vector<int>freq(26,0);
+        vector<int> freq(26, 0); // Declare and initialize the frequency vector
 
-       for (int i = 0; i < n; i++) { // Count frequency of characters in string s
+        for (int i = 0; i < n; i++) { // Count frequency of characters in string s
             char ch = s[i];
             freq[ch - 'a']++;
         }
-int oddfreq=0;
-int evenfreq=n+1;
-        for(int i=0; i<n ; i++){
-            if(freq[i]==0) continue;
 
-            if (freq[i]%2 ==0){
-                evenfreq= min(evenfreq,freq[i]);
-            }
-            else{
-             oddfreq= max(oddfreq,freq[i]);
+        int oddfreq = 0;
+        int evenfreq = n + 1;
+
+        for (int i = 0; i < 26; i++) { // Loop over all 26 characters
+            if (freq[i] == 0) continue;
+
+            if (freq[i] % 2 == 0) {
+                evenfreq = min(evenfreq, freq[i]);
+            } else {
+                oddfreq = max(oddfreq, freq[i]);
             }
         }
-         return oddfreq-evenfreq;
+
+        return oddfreq - evenfreq;
     }
 };
