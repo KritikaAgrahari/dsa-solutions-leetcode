a63ecaf349ad96a717ed9e3533e6ab7722eb3487
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2026-06-19 21:03:52 +0545

    Time: 0 ms (100%), Space: 8.4 MB (64.81%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,48 +1,20 @@
 class Solution {
 public:
-    bool backspaceCompare(string s, string t) {
-        int i = s.length() - 1;
-        int j = t.length() - 1;
-        int skip_s = 0, skip_t = 0;
+    string build(string str) {
+        string res;
 
-        while (i >= 0 || j >= 0) {
-            // Process backspaces in s
-            while (i >= 0) {
-                if (s[i] == '#') {
-                    skip_s++;
-                    i--;
-                } else if (skip_s > 0) {
-                    skip_s--;
-                    i--;
-                } else {
-                    break;
-                }
+        for (char c : str) {
+            if (c == '#') {
+                if (!res.empty()) res.pop_back();
+            } else {
+                res.push_back(c);
             }
-
-            // Process backspaces in t
-            while (j >= 0) {
-                if (t[j] == '#') {
-                    skip_t++;
-                    j--;
-                } else if (skip_t > 0) {
-                    skip_t--;
-                    j--;
-                } else {
-                    break;
-                }
-            }
-
-            // Compare characters after backspace handling
-            char first = (i >= 0) ? s[i] : '$';
-            char second = (j >= 0) ? t[j] : '$';
-
-            if (first != second) return false;
-
-            // Move to the next character
-            i--;
-            j--;
         }
 
-        return true;
+        return res;
+    }
+
+    bool backspaceCompare(string s, string t) {
+        return build(s) == build(t);
     }
 };
