116b0c6995d9113ef39c139627dae280897f04e8
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2026-06-23 20:21:59 +0545

    Time: 0 ms (100%), Space: 14.6 MB (28.97%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,49 +1,15 @@
 class Solution {
 public:
-
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        
-       
-        // Sort both arrays
-        std::sort(nums1.begin(), nums1.end());
-        std::sort(nums2.begin(), nums2.end());
-        
-        std::vector<int> result;
-        int i = 0, j = 0;
-        
-        // Use two pointers to find intersections
-        while (i < nums1.size() && j < nums2.size()) {
-            if (nums1[i] < nums2[j]) {
-                ++i;
-            } else if (nums1[i] > nums2[j]) {
-                ++j;
-            } else {
-                // Skip duplicates
-                if (result.empty() || result.back() != nums1[i]) {
-                    result.push_back(nums1[i]);
-                }
-                ++i;
-                ++j;
+        unordered_set<int> s1(nums1.begin(), nums1.end());
+        unordered_set<int> resultSet;
+
+        for (int num : nums2) {
+            if (s1.count(num)) {
+                resultSet.insert(num);
             }
         }
-        
-        return result;
-    }
 
-
-int main() {
-    Solution solution;
-    std::vector<int> nums1 = {1, 2, 2, 1};
-    std::vector<int> nums2 = {2, 2};
-    
-    std::vector<int> result = solution.intersection(nums1, nums2);
-    
-    std::cout << "Intersection: ";
-    for (int num : result) {
-        std::cout << num << " ";
+        return vector<int>(resultSet.begin(), resultSet.end());
     }
-    std::cout << std::endl;
-    
-    return 0;
-}
 };
