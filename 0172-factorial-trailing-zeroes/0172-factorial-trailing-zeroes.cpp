b4fb0f33d1dd91821c3b2bfdc1528de8f23cb995
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2026-07-31 16:03:00 +0530

    Time: 0 ms (100%), Space: 8 MB (99.66%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+
+        int count = 0;
+
+        while(n > 0) {
+
+            n = n / 5;      // Count multiples of 5,25,125,...
+
+            count += n;
+        }
+
+        return count;
+    }
+};
