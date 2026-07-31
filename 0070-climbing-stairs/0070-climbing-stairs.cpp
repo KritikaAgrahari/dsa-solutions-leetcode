bae5a360b21e9c97f7f5894d7a10d83ea8cf5ebb
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2026-07-31 20:19:06 +0530

    Time: 0 ms (100%), Space: 8 MB (58.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n==1 || n==2)
-        return n;
-        int a=1;
-        int b=2;
-        int c=3;
-        for (int i=3;i<=n; i++){
-            c= b + a;
-            int temp_b=b;
-            b=c;
-            a=temp_b;
+
+        if (n <= 2)
+            return n;
+
+        int first = 1;
+        int second = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
         }
-return c;
-        
+
+        return second;
     }
 };
