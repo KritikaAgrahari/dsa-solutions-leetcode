e699a4d97d72937aba41feb1e5d7ded89ab4cfd7
Kritika Agrahari <143201675+KritikaAgrahari@users.noreply.github.com>
2026-07-31 20:16:39 +0530

    Time: 0 ms (100%), Space: 8.7 MB (15.13%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if (x == 0) return 0; // Edge case for 0
-        
-        int low = 1, high = x, ans = 0; 
-        while (low <= high) {
-            int mid = low + (high - low) / 2; // Avoids overflow
-            long long sqr = (long long)mid * mid; // Prevent overflow
-            
-            if (sqr == x) return mid; 
-            else if (sqr < x) {
-                ans = mid; // Store the answer
-                low = mid + 1; // Search in the right half
-            } else {
-                high = mid - 1; // Search in the left half
+
+        if (x == 0 || x == 1)
+            return x;
+
+        long long left = 1;
+        long long right = x;
+        int ans = 0;
+
+        while (left <= right) {
+
+            long long mid = left + (right - left) / 2;
+
+            if (mid * mid == x)
+                return mid;
+
+            else if (mid * mid < x) {
+                ans = mid;
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
             }
         }
-        return ans; // Returns the floor value of sqrt(x)
+
+        return ans;
     }
 };
